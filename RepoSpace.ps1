9c4c6c2a9bde53d499c8351cdbf44d594c1dc103
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-03-29 10:40:19 -0500

    Add files via upload

---
+++ RepoSpace.ps1
@@ -0,0 +1,9 @@
+﻿$x = Get-VBRBackupRepository -ScaleOut
+foreach ($repo in $x) {
+    echo $repo.Name
+    $SOBRExtents = Get-VBRRepositoryExtent -Repository $repo.Name
+    foreach ($extent in $SOBRExtents) {
+        Write-Host "   - " $extent.Name ($extent.Repository.GetContainer().CachedTotalSpace.InBytes / 1GB) ($extent.Repository.GetContainer().CachedFreeSpace.InBytes / 1GB)
+        Write-Host "   - " $extent.Name ($extent.Repository.GetContainer().CachedUsedSpace.InBytes / 1GB)
+    }
+}
