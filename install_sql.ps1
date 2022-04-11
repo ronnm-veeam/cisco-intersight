5144106b1fd958bdd7fd4d21f56d3a4cbc63a552
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-04-11 10:54:52 -0500

    Update install_sql.ps1

--- install_sql.ps1
+++ install_sql.ps1
@@ -34,27 +34,27 @@ $sqltest = Get-Service | where { $_.ServiceName -eq 'MSSQL $sqlinstancename'  }
 if ($sqltest.count -eq 0 ) {
     if (-not (Test-Path $sqlsetup))
     {
-        Write-host "Veeam SQL Express installer not found!"
+        Write-host 'Veeam SQL Express installer not found!'
         exit 1
     }
     $arguments="/q /Action=Install /HideConsole=1  /Features=SQL,Tools /InstanceName=`"$sqlinstancename`" /SQLSYSADMINACCOUNTS=`"$srvdomuser`" /SQLSVCACCOUNT=`"$srvdomuser`" /SQLSVCPASSWORD=`"$srvpasswd`" /TCPENABLED=1 /NPENABLED=1 /IAcceptSQLServerLicenseTerms=1 /UpdateEnabled=`"$false`""
-    Write-host "Installing $sqlsetup"
+    Write-host 'Installing $sqlsetup'
     
     $securePassword = ConvertTo-SecureString -String $srvpasswd -AsPlainText -Force
     $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $srvdomuser, $securePassword
     
     Start-Process -file $sqlsetup -arg $arguments -LoadUserProfile -passthru -Credential $cred | wait-process
     
-    Write-host "Installation completed $sqlsetup"
+    Write-host 'Installation completed $sqlsetup'
 }
 else
 {
     write-host "$sqlinstancename is already installed, skipping"
 }
 
-$sqltest = Get-Service | where { $_.ServiceName -eq "MSSQL`$$sqlinstancename"  }
-if ($sqltest.count -gt 0 -and $sqltest[0].Status -ne "running" )
+$sqltest = Get-Service | where { $_.ServiceName -eq 'MSSQL $sqlinstancename'  }
+if ($sqltest.count -gt 0 -and $sqltest[0].Status -ne 'running' )
 {
-    write-host "Couldn't find installed $sqlinstancename so might install might have failed"
-    write-host "Check Program Files\Microsoft SQL Server\100\Setup Bootstrap\Log for error"
+    write-host 'Couldn't find installed $sqlinstancename so might install might have failed'
+    write-host 'Check Program Files\Microsoft SQL Server\100\Setup Bootstrap\Log for error'
 }
