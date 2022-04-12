a4a1eb57a17ee0411c4a741775ce90a1b255f9ab
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-04-12 14:45:46 -0500

    Update install_sql.ps1

--- install_sql.ps1
+++ install_sql.ps1
@@ -43,7 +43,7 @@ if ($sqltest.count -eq 0 ) {
     $securePassword = ConvertTo-SecureString -String $srvpasswd -AsPlainText -Force
     $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $srvdomuser, $securePassword
     
-    Write-Host "Start-Process -file $sqlsetup -arg $arguments -LoadUserProfile -passthru -Credential $cred | wait-process" *>> $logdir"\install_sql.log"
+    Write-Host "Start-Process -FilePath $sqlsetup -arg $arguments -LoadUserProfile -passthru -Credential $cred | wait-process" *>> $logdir"\install_sql.log"
     Start-Process -FilePath $sqlsetup -arg $arguments -LoadUserProfile -passthru -Credential $cred | wait-process *>> $logdir"\install_sql.log"
     
     Write-host "Installation completed $sqlsetup" *>> $logdir"\install_sql.log"
