9e62e727d26f1c25643ef957a62f950c80d0d392
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-04-12 16:55:52 -0500

    Update install_sql.ps1

--- install_sql.ps1
+++ install_sql.ps1
@@ -45,8 +45,8 @@ if ($sqltest.count -eq 0 ) {
     $securePassword = ConvertTo-SecureString -String $srvpasswd -AsPlainText -Force
     $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $srvdomuser, $securePassword
     
-    Write-Host "Start-Process -FilePath $sqlsetup -arg $arguments -PassThru -LoadUserProfile | Wait-Process" *>> $logdir"\install_sql.log"
-    Start-Process -FilePath $sqlsetup -arg $arguments -PassThru -LoadUserProfile | Wait-Process *>> $logdir"\install_sql.log"
+    Write-Host "Start-Process -FilePath $sqlsetup -arg $arguments -PassThru -LoadUserProfile -WorkingDirectory $logdir | Wait-Process" *>> $logdir"\install_sql.log"
+    Start-Process -FilePath $sqlsetup -arg $arguments -PassThru -LoadUserProfile -WorkingDirectory $logdir | Wait-Process *>> $logdir"\install_sql.log"
     #Write-Host "Start-Process -FilePath $sqlsetup -arg $arguments -LoadUserProfile -passthru -Credential $cred | wait-process" *>> $logdir"\install_sql.log"
     #Start-Process -FilePath $sqlsetup -arg $arguments -LoadUserProfile -passthru -Credential $cred | wait-process *>> $logdir"\install_sql.log"
     
