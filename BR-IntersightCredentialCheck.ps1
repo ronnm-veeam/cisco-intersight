b42cf6be0805031839b35148edc69b8a887638de
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-05-05 11:04:15 -0500

    Add files via upload

---
+++ BR-IntersightCredentialCheck.ps1
@@ -0,0 +1,16 @@
+﻿Param
+(
+[Parameter(Mandatory)][string]$username,
+[Parameter(Mandatory)][string]$password,
+[Parameter(Mandatory)][string]$credentialDesc
+)
+
+try {
+    $cred = Get-VBRCredentials | Where-Object {$_.Description -eq "$credentialDesc" -and $_.Name -eq "$username"}
+    if ($cred -EQ $null) {
+        $cred = Add-VBRCredentials -User "$username" -Password "$password" -Description "$credentialDesc"
+        if ($cred -EQ $null) { exit 1 }
+    }
+    exit 0
+}
+catch { exit 1 }
