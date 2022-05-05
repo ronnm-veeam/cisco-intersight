347b333f7787bfa1e7745f850201bf54f62abb32
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-05-05 17:11:58 -0400

    Add files via upload

---
+++ BR-IntersightAddProxy.ps1
@@ -0,0 +1,30 @@
+﻿Param
+(
+[Parameter(Mandatory)][string]$serverName,
+[Parameter(Mandatory)][int]$proxyType
+)
+
+try {
+    $managedServer = Get-VBRServer -Name $serverName
+    if ($managedServer -NE $null) {
+        Switch ($proxyType) {
+            0 {
+                Write-Host "Adding VMWare proxy"
+                Add-VBRViProxy -Server $serverName
+            }
+            default {
+                Write-Host "Proxy type not supported!"
+                exit 1
+            }
+        }
+        exit 0 
+    }
+    else {
+        Write-Output "Managed server not found!"
+        exit 0
+    }
+}
+catch {
+    Write-Output "Exception in add proxy"
+    exit 1
+}
