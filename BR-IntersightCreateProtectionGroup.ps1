c16c401974e0e5f8e65e415407d72c95561fd7c3
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-05-05 17:35:01 -0400

    Add files via upload

---
+++ BR-IntersightCreateProtectionGroup.ps1
@@ -0,0 +1,35 @@
+﻿Param
+(
+[Parameter(Mandatory)][string]$protectionGroupName,
+[Parameter(Mandatory)][string]$protectionGroupDesc,
+[Parameter(Mandatory)][string]$agentTarget,
+[Parameter(Mandatory)][string]$userName,
+[Parameter(Mandatory)][string]$userDesc
+)
+
+try {
+    $cred = Get-VBRCredentials | Where-Object {$_.Description -eq "$userDesc" -and $_.Name -eq "$userName"}
+    if ($cred -ne $null) {
+        $customCredsArray = @()
+        $icCreds = New-VBRIndividualComputerCustomCredentials -HostName $agentTarget -Credentials $cred
+        $customCredsArray += $icCreds
+
+        $protContainer = New-VBRIndividualComputerContainer -CustomCredentials $customCredsArray
+ 
+        $protGroup = Add-VBRProtectionGroup -Name "$protectionGroupName" -Container $protContainer -Description "$protectionGroupDesc"
+
+        $deploymentOpt = New-VBRProtectionGroupDeploymentOptions -InstallAgent -UpgradeAutomatically -RebootIfRequired
+
+        Set-VBRProtectionGroup -ProtectionGroup $protGroup -DeploymentOptions $deploymentOpt
+
+        Rescan-VBREntity -Entity $protGroup   
+    }
+    else {
+        Write-Output "Credentials not found!"
+        exit 1
+    }
+}
+catch {
+    Write-Output "Exception in add protection group"
+    exit 1
+}
