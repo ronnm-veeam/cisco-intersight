8808e93efe8456907961bc454b4643efe98aa4fd
Ronn Martin <79283547+ronnm-veeam@users.noreply.github.com>
2022-05-06 09:26:15 -0400

    Add files via upload

---
+++ BR-IntersightCreatePGBackupJob.ps1
@@ -0,0 +1,42 @@
+﻿Param
+(
+[Parameter(Mandatory)][string]$protectionGroupName,
+[Parameter(Mandatory)][string]$backupJobName,
+[Parameter(Mandatory)][string]$backupRepo,
+[Parameter(Mandatory)][string]$agentPlatform
+)
+
+try {
+    $protectionGroup = Get-VBRProtectionGroup -Name $protectionGroupName
+
+    $repository = Get-VBRBackupRepository -Name $backupRepo
+
+    $daily = New-VBRDailyOptions -DayOfWeek Friday -Period 23:00
+
+    switch ($agentPlatform)
+    {
+        0 {
+            $schedule = New-VBRServerScheduleOptions -Type Daily -DailyOptions $daily
+            Add-VBRComputerBackupJob -OSPlatform Windows -Type Server -Mode ManagedByBackupServer -BackupObject $protectionGroup `
+              -BackupType EntireComputer -Name "$backupJobName" -Description "Cisco Intersight-automated agent backup" `
+              -BackupRepository $repository -EnableSchedule -ScheduleOptions $schedule
+        }
+
+        1 {
+            $schedule = New-VBRServerScheduleOptions -Type Daily -DailyOptions $daily
+            Add-VBRComputerBackupJob -OSPlatform Linux -Type Server -Mode ManagedByBackupServer -BackupObject $protectionGroup `
+              -BackupType EntireComputer -Name $backupJobName -Description "Cisco Intersight-automated agent backup" `
+              -BackupRepository $repository -EnableSchedule -ScheduleOptions $schedule
+        }
+
+        Default {
+            Write-Output "Unsupported platform"
+            exit 1
+        }
+    }
+    exit 0
+}
+catch {
+    Write-Output "Exception in add protection group backup job"
+    exit 1
+}
